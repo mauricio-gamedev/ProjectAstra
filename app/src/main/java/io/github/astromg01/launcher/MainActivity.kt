90fbf48dd85ac58437b436f3d9ce8fb872c5e791
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-21 00:48:11 -0400

    Expose temporary native Surface test control

--- app/src/main/java/io/github/astromg01/launcher/MainActivity.kt
+++ app/src/main/java/io/github/astromg01/launcher/MainActivity.kt
@@ -1,8 +1,18 @@
 package io.github.astromg01.launcher
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.astromg01.launcher.game.GameSurfaceActivity
 import io.github.astromg01.launcher.ui.AstraApp
 import io.github.astromg01.launcher.ui.AstraTheme
 
@@ -11,7 +21,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AstraTheme {
-                AstraApp()
+                Box(Modifier.fillMaxSize()) {
+                    AstraApp()
+                    Button(
+                        onClick = {
+                            startActivity(Intent(this@MainActivity, GameSurfaceActivity::class.java))
+                        },
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(end = 14.dp, bottom = 94.dp)
+                    ) {
+                        Text("Surface test")
+                    }
+                }
             }
         }
     }
