1e4d789d9ae039553cb75cc4de2f4d907b4f27c4
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-21 11:33:17 -0400

    Bump Project Astra to alpha18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.astromg01.launcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.1.0-alpha17"
+        versionCode = 18
+        versionName = "0.1.0-alpha18"
         buildConfigField(
             "String",
             "SIGNING_MODE",
