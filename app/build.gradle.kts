0f647cfd1f70198e07b43b5a840cdd5f40bde6ad
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-21 22:59:48 -0400

    Bump integration build to alpha39

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.astromg01.launcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.1.0-alpha19"
+        versionCode = 39
+        versionName = "0.1.0-alpha39"
         buildConfigField(
             "String",
             "SIGNING_MODE",
