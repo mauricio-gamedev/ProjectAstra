b709e234874bdb5c7b525bcf91afa28fc8a6330b
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-21 23:40:07 -0400

    Bump loader classpath fix to alpha41

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.astromg01.launcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 40
-        versionName = "0.1.0-alpha40"
+        versionCode = 41
+        versionName = "0.1.0-alpha41"
         buildConfigField(
             "String",
             "SIGNING_MODE",
