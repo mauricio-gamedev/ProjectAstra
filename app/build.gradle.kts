b3be80e88e4314981c1839b35e2256afa8894759
Mauricio.dev <antoniomauriciorodriguesalves@gmail.com>
2026-08-21 23:19:48 -0400

    Bump loader parser fix to alpha40

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.github.astromg01.launcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 39
-        versionName = "0.1.0-alpha39"
+        versionCode = 40
+        versionName = "0.1.0-alpha40"
         buildConfigField(
             "String",
             "SIGNING_MODE",
